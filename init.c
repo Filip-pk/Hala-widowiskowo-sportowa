9d4ea3dcb1b502e5511a0aabaf64acc2b29f8277
Filip <turczakfilip63@gmail.com>
2025-12-22 11:31:30 +0100

    Pierwsza cegła: pamięć współdzielona

--- init.c
+++ init.c
@@ -1,2 +1,24 @@
 #include "common.h"
-int main() { return 0; }
+
+int main() {
+    printf("--- SETUP ---\n");
+
+    int shmid = shmget(KEY_SHM, sizeof(SharedState), IPC_CREAT | 0600);
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    SharedState *stan = (SharedState*)shmat(shmid, NULL, 0);
+    if (stan == (void*)-1) {
+        perror("shmat");
+        shmctl(shmid, IPC_RMID, NULL);
+        exit(1);
+    }
+
+    memset(stan, 0, sizeof(SharedState));
+    shmdt(stan);
+
+    printf("[OK] SHM utworzone.\n");
+    return 0;
+}
