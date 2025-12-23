12bfee3053aafdffca52fba4768a14129a4ea310
Filip <turczakfilip63@gmail.com>
2025-12-23 20:17:42 +0100

    Main odpala ekipę: procesy + generator kibiców

--- main.c
+++ main.c
@@ -1,2 +1,76 @@
 #include "common.h"
-int main() { return 0; }
+#include <sys/wait.h>
+
+#define MAX_PROC K
+
+int main() {
+    setbuf(stdout, NULL);
+
+    int shmid = shmget(KEY_SHM, sizeof(SharedState), 0600);
+    if (shmid == -1) {
+        perror("shmget");
+        fprintf(stderr, "Uruchom najpierw ./setup\n");
+        exit(1);
+    }
+
+    int semid = semget(KEY_SEM, 0, 0600);
+    if (semid == -1) { perror("semget"); exit(1); }
+
+    int msgid = msgget(KEY_MSG, 0600);
+    if (msgid == -1) { perror("msgget"); exit(1); }
+
+    SharedState *stan = (SharedState*)shmat(shmid, NULL, 0);
+    if (stan == (void*)-1) { perror("shmat"); exit(1); }
+
+    printf("--- START SYMULACJI ---\n");
+
+    if (!fork()) {
+        execl("./kierownik", "kierownik", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    for (int i = 0; i < LICZBA_SEKTOROW; i++)
+        if (!fork()) {
+            char b[10];
+            sprintf(b, "%d", i);
+            execl("./pracownik", "pracownik", b, NULL);
+            perror("execl");
+            exit(1);
+        }
+
+    for (int i = 0; i < LICZBA_KAS; i++)
+        if (!fork()) {
+            char b[10];
+            sprintf(b, "%d", i);
+            execl("./kasjer", "kasjer", b, NULL);
+            perror("execl");
+            exit(1);
+        }
+
+    int total_kibicow = (int)(K * 0.2);
+    int active = 0;
+
+    srand(time(NULL));
+    sleep(1);
+
+    for (int i = 0; i < total_kibicow; i++) {
+        while (waitpid(-1, NULL, WNOHANG) > 0) active--;
+        if (active >= MAX_PROC) { wait(NULL); active--; }
+
+        if (!fork()) {
+            char id[20];
+            sprintf(id, "%d", i);
+            execl("./kibic", "kibic", id, "0", NULL);
+            perror("execl");
+            exit(1);
+        }
+
+        active++;
+        usleep(10000);
+    }
+
+    while (wait(NULL) > 0);
+    shmdt(stan);
+    return 0;
+}
