369ff05e5793a0f7e4c761eebbeea8a7ac11af91
Filip <turczakfilip63@gmail.com>
2025-12-24 19:14:12 +0100

    Kasy: 1–2 bilety + SOLD OUT

--- kasjer.c
+++ kasjer.c
@@ -16,16 +16,16 @@ int main(int argc, char *argv[]) {
     srand(time(NULL) + id);
 
     int shmid = shmget(KEY_SHM, sizeof(SharedState), 0600);
-    if (shmid == -1) { perror("shmget"); exit(1); }
+    if (shmid == -1) exit(1);
 
     int semid = semget(KEY_SEM, 0, 0600);
-    if (semid == -1) { perror("semget"); exit(1); }
+    if (semid == -1) exit(1);
 
     int msgid = msgget(KEY_MSG, 0600);
-    if (msgid == -1) { perror("msgget"); exit(1); }
+    if (msgid == -1) exit(1);
 
     SharedState *stan = (SharedState*)shmat(shmid, NULL, 0);
-    if (stan == (void*)-1) { perror("shmat"); exit(1); }
+    if (stan == (void*)-1) exit(1);
 
     int limit_sektor = K / 8;
 
@@ -47,23 +47,34 @@ int main(int argc, char *argv[]) {
         usleep(100000);
 
         int sektor = -1;
-        int start = rand() % LICZBA_SEKTOROW;
 
         sem_op(semid, SEM_SHM, -1);
+        int start = rand() % LICZBA_SEKTOROW;
         for (int i = 0; i < LICZBA_SEKTOROW; i++) {
             int s = (start + i) % LICZBA_SEKTOROW;
             if (stan->sprzedane_bilety[s] < limit_sektor) {
-                stan->sprzedane_bilety[s]++;
+                int ile = (rand() % 2) + 1;
+                if (stan->sprzedane_bilety[s] + ile <= limit_sektor) stan->sprzedane_bilety[s] += ile;
+                else stan->sprzedane_bilety[s]++;
                 sektor = s;
                 break;
             }
         }
         sem_op(semid, SEM_SHM, 1);
 
+        if (sektor == -1) {
+            stan->aktywne_kasy[id] = 0;
+            printf("[KASA %d] SOLD OUT\n", id);
+            fflush(stdout);
+        } else {
+            printf("[KASA %d] Sprzedano bilet do sektora %d\n", id, sektor);
+            fflush(stdout);
+        }
+
         MsgBilet msg;
         msg.mtype = 999;
         msg.sektor_id = sektor;
-        if (msgsnd(msgid, &msg, sizeof(int), 0) == -1) perror("msgsnd");
+        msgsnd(msgid, &msg, sizeof(int), 0);
     }
 
     shmdt(stan);
