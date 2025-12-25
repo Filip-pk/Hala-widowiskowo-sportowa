186574d215be188cfd4732fa7a6a738f9e60640c
Filip <turczakfilip63@gmail.com>
2025-12-25 15:23:46 +0100

    Sygnał 3: ewakuacja i zbieranie raportów

--- kierownik.c
+++ kierownik.c
@@ -1,23 +1,46 @@
 #include "common.h"
 #include <sys/wait.h>
 
+static void ewakuacja(int msgid, SharedState *stan) {
+    stan->ewakuacja_trwa = 1;
+    stan->status_meczu = 2;
+
+    for (int i = 0; i < LICZBA_SEKTOROW; i++) {
+        MsgSterujacy msg = {10 + i, 3, i};
+        msgsnd(msgid, &msg, sizeof(int) * 2, 0);
+    }
+
+    int raporty = 0;
+    while (raporty < LICZBA_SEKTOROW) {
+        MsgSterujacy rap;
+        if (msgrcv(msgid, &rap, sizeof(int) * 2, 99, 0) >= 0) {
+            printf("[RAPORT] Sektor %d pusty\n", rap.sektor_id);
+            fflush(stdout);
+            raporty++;
+        }
+    }
+
+    printf("[KIEROWNIK] Koniec\n");
+    fflush(stdout);
+}
+
 int main() {
     setbuf(stdout, NULL);
 
     int msgid = msgget(KEY_MSG, 0600);
-    if (msgid == -1) { perror("msgget"); exit(1); }
+    if (msgid == -1) exit(1);
 
     int shmid = shmget(KEY_SHM, sizeof(SharedState), 0600);
-    if (shmid == -1) { perror("shmget"); exit(1); }
+    if (shmid == -1) exit(1);
 
     SharedState *stan = (SharedState*)shmat(shmid, NULL, 0);
-    if (stan == (void*)-1) { perror("shmat"); exit(1); }
+    if (stan == (void*)-1) exit(1);
 
     stan->status_meczu = 0;
     stan->czas_pozostaly = CZAS_PRZED_MECZEM;
 
-    pid_t zegar = fork();
-    if (zegar == 0) {
+    pid_t zegar_pid = fork();
+    if (zegar_pid == 0) {
         time_t start = time(NULL);
         while (1) {
             int left = CZAS_PRZED_MECZEM - (int)(time(NULL) - start);
@@ -42,21 +65,30 @@ int main() {
         exit(0);
     }
 
-    printf("--- KIEROWNIK URUCHOMIONY ---\n");
-    printf("Komendy: 1-Stop sektora, 2-Start sektora\n");
+    printf("Komendy: 1-stop sektora, 2-start sektora, 3-ewakuacja\n");
     fflush(stdout);
 
     while (1) {
-        if (waitpid(zegar, NULL, WNOHANG) > 0) break;
+        if (waitpid(zegar_pid, NULL, WNOHANG) > 0) {
+            ewakuacja(msgid, stan);
+            break;
+        }
 
-        int cmd, s;
+        int cmd;
         if (scanf("%d", &cmd) == 1) {
+            if (cmd == 3) {
+                kill(zegar_pid, SIGTERM);
+                waitpid(zegar_pid, NULL, 0);
+                ewakuacja(msgid, stan);
+                break;
+            }
             if (cmd == 1 || cmd == 2) {
-                printf("Podaj nr sektora (0-7): ");
+                int s;
+                printf("Sektor (0-7): ");
                 fflush(stdout);
                 if (scanf("%d", &s) == 1 && s >= 0 && s < LICZBA_SEKTOROW) {
                     MsgSterujacy msg = {10 + s, cmd, s};
-                    if (msgsnd(msgid, &msg, sizeof(int) * 2, 0) == -1) perror("msgsnd");
+                    msgsnd(msgid, &msg, sizeof(int) * 2, 0);
                 }
             }
         }
