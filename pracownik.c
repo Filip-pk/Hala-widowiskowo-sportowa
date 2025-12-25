c628e44fa2b649a0c8fc911ed32252c7dce915ec
Filip <turczakfilip63@gmail.com>
2025-12-25 17:16:45 +0100

    Sektory puste: pracownicy raportują

--- pracownik.c
+++ pracownik.c
@@ -9,32 +9,44 @@ int main(int argc, char *argv[]) {
     int sektor = atoi(argv[1]);
 
     int shmid = shmget(KEY_SHM, sizeof(SharedState), 0600);
-    if (shmid == -1) { perror("shmget"); exit(1); }
+    if (shmid == -1) exit(1);
 
     int msgid = msgget(KEY_MSG, 0600);
-    if (msgid == -1) { perror("msgget"); exit(1); }
+    if (msgid == -1) exit(1);
 
     SharedState *stan = (SharedState*)shmat(shmid, NULL, 0);
-    if (stan == (void*)-1) { perror("shmat"); exit(1); }
+    if (stan == (void*)-1) exit(1);
 
     long my_type = 10 + sektor;
 
     while (1) {
         MsgSterujacy msg;
-        if (msgrcv(msgid, &msg, sizeof(int) * 2, my_type, 0) == -1) {
-            if (errno == EINTR) continue;
-            perror("msgrcv");
-            break;
-        }
+        if (msgrcv(msgid, &msg, sizeof(int) * 2, my_type, 0) == -1) break;
 
         if (msg.typ_sygnalu == 1) {
             stan->blokada_sektora[sektor] = 1;
-            printf("[TECH %d] Sygnał 1: Blokada wejścia.\n", sektor);
+            printf("[TECH %d] Sygnał 1\n", sektor);
             fflush(stdout);
         } else if (msg.typ_sygnalu == 2) {
             stan->blokada_sektora[sektor] = 0;
-            printf("[TECH %d] Sygnał 2: Wznowienie wejścia.\n", sektor);
+            printf("[TECH %d] Sygnał 2\n", sektor);
             fflush(stdout);
+        } else if (msg.typ_sygnalu == 3) {
+            printf("[TECH %d] Sygnał 3\n", sektor);
+            fflush(stdout);
+
+            while (stan->bramki[sektor][0].zajetosc > 0 || stan->bramki[sektor][1].zajetosc > 0)
+                usleep(100000);
+
+            MsgSterujacy rap;
+            rap.mtype = 99;
+            rap.typ_sygnalu = 0;
+            rap.sektor_id = sektor;
+            msgsnd(msgid, &rap, sizeof(int) * 2, 0);
+
+            printf("[TECH %d] Raport wysłany\n", sektor);
+            fflush(stdout);
+            break;
         }
     }
 
